864ed710edbe44267c26faa0f09b247f185ab638
Ramin Faracov <ramin.faracov@gmail.com>
2022-04-04 00:22:57 +0400

    Update RegularExpression_MultiReplaceSample.sql

--- RegularExpression_MultiReplaceSample.sql
+++ RegularExpression_MultiReplaceSample.sql
@@ -1,21 +1,24 @@
+/****************************************************************************** 
+**************** Replacing only words that start with numbers *****************
+******************************************************************************/
 with tbl as ( 
-	select 'Clowns have taken over the world    Authors Name   3h' as ptext
+	select 'Clowns have taken over the world Authors Name 3h' as ptext
 	union all 
-	select 'My test My Test 25m'  as ptext
+	select 'My test My Test 25m My Test'  as ptext
 	union all 
-	select 'test test test 32s test test'  as ptext
+	select 'Test test test 32s test test'  as ptext
 )
 select
-	ptext, 
 	REGEXP_REPLACE(ptext, '(?=[^ ]*[a-z])(?=[^ ]*[0-9])[^ ]+', '', 'g') as newtext 
 from tbl
--- Result: 
 
-ptext														newtext
----------------------------------------------------------------------------------------------------------------
-Clowns have taken over the world    Authors Name   3h		Clowns have taken over the world    Authors Name   
-My test My Test 25m											My test My Test 
-test test test 32s test test								test test test  test test
+/*
+Result:
 
+newtext
+---------------------------------------------------
+Clowns have taken over the world Authors Name
+My test My Test  My Test
+Test test test  test test
 
-
+*/
